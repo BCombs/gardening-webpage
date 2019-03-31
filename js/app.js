cb3031e87f4bf973dbe147c8682da0c970a1cdcd
BCombs <bill@Bills-MacBook-Pro.local>
2019-03-31 13:53:44 -0400

    Added footer and nav functionality

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let currentSelection = document.getElementById('landing');
+let selectedButton = document.getElementById('landing-btn');
+selectedButton.style.color = 'yellow';
+
+// Change the content are when a button is selected
+onSelect = element => {
+  hideCurrentSelection();
+  showNewSelection(element);
+};
+
+// Hide what is currently showing
+hideCurrentSelection = () => {
+  currentSelection.classList.remove('selected');
+  // Change the selected button's color back to default
+  selectedButton.style.color = '#fff';
+};
+
+// Show the new selection
+showNewSelection = element => {
+  element.classList.add('selected');
+  currentSelection = element;
+  // Change the color of the newly selected button
+  selectedButton = document.getElementById(`${currentSelection.id}-btn`);
+  selectedButton.style.color = 'yellow';
+};
